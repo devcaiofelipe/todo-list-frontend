a0641e7b2067fe0658797582e04650749eca0fd4
Caio Felipe <devcaiofelipe15@gmail.com>
2023-03-25 07:14:17 -0300

    feat: add border

--- src/components/Task/index.jsx
+++ src/components/Task/index.jsx
@@ -13,17 +13,20 @@ const Task = ({
   handleDoneTask }) => {
 
   return (
-    <li key={task.id} className="task-container" style={ task.done ? { backgroundColor: '#e8f3df' } : null }>
+    <div className="task-container-border"style={ checked ? { borderStyle: 'solid', borderWidth: '1px', borderColor: 'red', borderRadius: '5px' } : null }>
+      <li key={task.id} className="task-container" style={ task.done ? { backgroundColor: '#e8f3df' } : null }>
       <div>
-        <input type="checkbox" checked={checked} onChange={() => handleCheckTask(task.id)} className="task-container-checkbox"/>
-        <p className="messages">{description}</p>
-      </div>
+          <input type="checkbox" checked={checked} onChange={() => handleCheckTask(task.id)} className="task-container-checkbox"/>
+          <p className="messages">{description}</p>
+        </div>
       <div>
         <button className="task-container-button done-button" onClick={() => handleDoneTask(task.id)}>{ task.done ? <MdRemoveDone/> : <MdDone/>}</button>
         <button className="task-container-button update-button" onClick={() => handleTaskIdToUpdate(task.id)}><BsPencil/></button>
         <button className="task-container-button confirm-button" onClick={() => handleTaskToDelete(task.id)}><BsTrash/></button>
       </div>
     </li>
+    </div>
+    
   )
 }
 
