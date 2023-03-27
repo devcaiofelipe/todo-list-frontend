7143b019fdc64213b68026e2b3be5d13984cd886
Caio Felipe <devcaiofelipe15@gmail.com>
2023-03-27 18:56:58 -0300

    feat: login screen

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,25 @@
+import './styles.css'
+import { RiTodoLine } from "react-icons/ri";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+
 const Login = () => {
   return (
-    <div class="container">
-      <h1>Oxe maluco</h1>
+    <div className="container">
+      <header className="header-login">
+        <RiTodoLine></RiTodoLine>
+        <p>Caio`s TD List</p>
+      </header>
+      <div className="login-container">
+        <div className="login-content">
+          <p className="login-title">Login</p>
+          <p className="login-info">Type your e-mail and password below to sign-in at To Do List</p>
+          <form action="#" className="login-form">
+            <input type="text" className="form-input" placeholder="E-mail"/>
+            <input type="password" className="form-input" placeholder="Password"/>
+          </form>
+          <button type="submit" className="signin-button">Sign in</button>
+        </div>
+      </div>
     </div>
   )
 }
